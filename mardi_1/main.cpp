d8593670c8693a98198514928dc0c658b4dc0896
stbn1699 <esteban.basson@ecole-isitech.fr>
2022-12-06 14:04:04 +0100

    yfcivohvxutdtwiyfogcbijfsijfbiebfivabpgizieb

--- mardi_1/main.cpp
+++ mardi_1/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <sstream>
+#include <Windows.h>
+#include <vector>
+#include "Point.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main()
+{
+    auto* firstP = new Point(6, 9);
+    Point secondP;
+
+    cout << "firstP = (" << firstP->GetX() << ", " << firstP->GetY() << ")" << endl;
+    cout << "secondP = (" << secondP.GetX() << ", " << secondP.GetY() << ")" << endl;
+
+    delete firstP;
+
+    cout << "**** fin programme ***" << endl;
+
     return 0;
 }
